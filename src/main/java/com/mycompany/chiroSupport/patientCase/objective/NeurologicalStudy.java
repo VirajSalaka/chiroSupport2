4c2b49ff624280a9bcfe3ecb33d0996cfae494f0
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2017-04-14 22:52:23 +0530

    neurological and diagnosic studies are completed

---
+++ src/main/java/com/mycompany/chiroSupport/patientCase/objective/NeurologicalStudy.java
@@ -0,0 +1,91 @@
+package com.mycompany.chiroSupport.patientCase.objective;
+
+import com.mycompany.chiroSupport.patientCase.Examination;
+
+import javax.persistence.*;
+
+/**
+ * Created by Salaka on 4/14/2017.
+ */
+
+@Entity
+@Table(name="neurologicalstudy",
+        uniqueConstraints={@UniqueConstraint(columnNames={"id"})})
+public class NeurologicalStudy {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id", nullable=false, unique=true)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name="exam_id", nullable=false)
+    private Examination examination;
+
+    @Column(name="dateOfStudy")
+    private String dateOfStudy;
+
+    @Column(name="study_type", nullable=false)
+    private String studyType;
+
+    @Column(name="region")
+    private String region;
+
+    @Column(name="filename")
+    private String fileName;
+
+    @Column(name="impression")
+    private String impression;
+
+    public NeurologicalStudy(Examination examination){
+        this.examination=examination;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Examination getExamination() {
+        return examination;
+    }
+
+    public String getDateOfStudy() {
+        return dateOfStudy;
+    }
+
+    public void setDateOfStudy(String date) {
+        this.dateOfStudy = date;
+    }
+
+    public String getStudyType() {
+        return studyType;
+    }
+
+    public void setStudyType(String studyType) {
+        this.studyType = studyType;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getImpression() {
+        return impression;
+    }
+
+    public void setImpression(String impression) {
+        this.impression = impression;
+    }
+}
